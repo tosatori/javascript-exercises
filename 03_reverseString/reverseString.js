35265a0a34fdc4bd94b731e0eeffa41c5c5905c4
tosatori <tosatori@proton.me>
2022-08-04 10:08:14 +0200

    Solve exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Convert input String into CharArray
+    const input = string.split("");
 
+    // Reverse the order of elements in Array input
+    const output = input.reverse();
+    
+    // Convert chararcter Array to String without separators and return result
+    return output.join("");
 };
 
 // Do not edit below this line
