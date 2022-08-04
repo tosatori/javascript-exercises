6af8ee6dc56db77017e920672accbbb1def953ca
tosatori <tosatori@proton.me>
2022-08-04 20:07:31 +0200

    Simplify solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,6 @@
 const leapYears = function(year) {
-    if(year % 400 === 0 || year % 4 === 0 && year % 100 != 0) {
-        return true;
-    } else return false;
-};
+    return year % 400 === 0 || year % 4 === 0 && year % 100 != 0;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
