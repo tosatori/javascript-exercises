79a3a56a05d9c61508e3d79aa2c95401791e8faa
tosatori <tosatori@proton.me>
2022-08-04 10:52:06 +0200

    Solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
-};
+/* Pass Array input as first parameter, and Array args of variable length, indicaated by the three dots as second parameter. */
+const removeFromArray = function(input, ...args) {
+    // loop through every element in Array args
+    for (const i in args) {
+        //Find index number of the parameter element in Array input
+        const index = input.indexOf(args[i]);
+        // Remove parameter element in input Array
+        if(index > -1) {    // Only splice when element is found
+            input.splice(index, 1); // Second parameter specifies remove one element only
+        }
+        
+    }
+    return input;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
