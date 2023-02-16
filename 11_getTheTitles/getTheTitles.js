571b1ba10f795373cbdb8b5f30ed758318f61764
tosatori <tosatori@proton.me>
2023-02-16 16:51:11 +0100

    Solve Exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(input) {
+    let arr = [];
+    arr.push(input.forEach(element => {
+        return "title";
+    }))
+    return arr;
 };
 
 // Do not edit below this line
