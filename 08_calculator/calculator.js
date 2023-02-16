5af528b224b1034a9f37e8053b403e238face19c
tosatori <tosatori@proton.me>
2023-02-16 10:24:23 +0100

    Add factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,12 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	  if(a === 0) {
+      return 1;
+    } else {
+      return a * factorial(a - 1);
+    }
 };
 
 // Do not edit below this line
