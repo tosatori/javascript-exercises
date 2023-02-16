0f91d1311c26f3d9cc15344df4942147ccef5415
tosatori <tosatori@proton.me>
2023-02-16 11:11:13 +0100

    Solve Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+// RegExp to remove unwanted characters
+    let reg = /[^A-Za-z0-9]/g;
+
+// Remove unwanted and lower all characters
+    let lowStr = str.replace(reg, "").toLowerCase();
+
+// Split String into Array of characters, reverse them and 
+// join back to a String-Object
+    let rev = lowStr.split("").reverse().join("");
+
+// Compare Strings and return result
+    return lowStr === rev;
 
 };
 
