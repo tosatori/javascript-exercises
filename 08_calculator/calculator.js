78b55e5dd264622ccfbf08b18f31dbd30443f07a
tosatori <tosatori@proton.me>
2023-02-16 09:25:13 +0100

    Add power function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function(arr) {
   return arr.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
 const factorial = function() {
